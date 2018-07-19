3a56773d8bd48ed6f76f430530ceac58735cc34c
Raza Habib <rhabib01@gmail.com>
2018-07-19 18:10:51 +0100

    changed the hmc examples to use hmc and not rwm

--- examples/hmc_ring2d.py
+++ examples/hmc_ring2d.py
@@ -12,21 +12,22 @@ def prior(bs):
 
 
 if __name__ == '__main__':
-    from a_nice_mc.utils.statistics import NormalMonteCarloSampler
+    from a_nice_mc.utils.hmc import HamiltonianMonteCarloSampler
     from a_nice_mc.objectives.expression.ring2d import Ring2d
 
     os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
     energy_fn = Ring2d(display=True)
-    hmc = NormalMonteCarloSampler(energy_fn, prior)
-    z = hmc.sample(8000, 8000)
-    z = z[:, 3000:]
+    hmc = HamiltonianMonteCarloSampler(energy_fn, prior)
+    z = hmc.sample(800, 500)
+    z = z[:, 300:]
     z = np.reshape(z, [-1, 2])
     x, y = z[:, 0], z[:, 1]
     z = np.reshape(z, [-1, 2])
     plt.hist2d(x, y, bins=400)
     plt.xlim([-4, 4])
     plt.ylim([-4, 4])
+    plt.show()
     #plt.savefig('ring2d.png')
     print(np.mean(x))
     print(np.std(x))
